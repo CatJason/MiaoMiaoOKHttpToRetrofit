b0b313c26abd8f13ab70483e4bc861879f623ece
CatJason <CatJason@github.com>
2023-07-02 21:28:29 +0800

    使用 okhttp3

--- app/src/main/java/com/miao/miaomiaookhttptoretrofit/MainActivity.kt
+++ app/src/main/java/com/miao/miaomiaookhttptoretrofit/MainActivity.kt
@@ -1,13 +1,63 @@
 package com.miao.miaomiaookhttptoretrofit
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import androidx.appcompat.app.AppCompatActivity
+import okhttp3.*
+import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody.Companion.toRequestBody
 import java.io.IOException
 import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
+    companion object{
+        const val TIME = 5L
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun okHttpPost() {
+        val url = "https://example.com/api/users"
+        if (url.isBlank()) {
+            return
+        }
+        url.toHttpUrlOrNull() ?: return
+
+        val mediaType = "application/json; charset=utf-8".toMediaType()
+        val jsonString = ""
+        val requestBody = jsonString.toRequestBody(mediaType)
+
+        val headers = Headers.Builder().apply {
+            add("name", "value")
+            add("Content-Type", "application/json")
+            add("Accept", "application/json")
+        }.build()
+
+        val request = Request.Builder()
+            .headers(headers)
+            .post(requestBody)
+            .url(url)
+            .build()
+
+        val callback = object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+            }
+        }
+
+        OkHttpClient.Builder()
+            .connectTimeout(TIME, TimeUnit.SECONDS)
+            .readTimeout(TIME, TimeUnit.SECONDS)
+            .writeTimeout(TIME, TimeUnit.SECONDS)
+            .build()
+            .newCall(request)
+            .enqueue(callback)
     }
 }
