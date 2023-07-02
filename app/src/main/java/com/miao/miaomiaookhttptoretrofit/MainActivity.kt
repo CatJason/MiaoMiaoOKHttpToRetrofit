659cd539b954d59a2faae1077a11befc03ed0c7d
CatJason <CatJason@github.com>
2023-07-02 20:39:01 +0800

    init

---
+++ app/src/main/java/com/miao/miaomiaookhttptoretrofit/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.miao.miaomiaookhttptoretrofit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.io.IOException
+import java.util.concurrent.TimeUnit
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
